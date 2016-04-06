2f10dada4495497cd5c1f64b7d88e64c15f1059d
liuxiaobai <zwsibian@163.com>
2016-04-06 17:22:47 +0800

    Update spider.py

--- spider.py
+++ spider.py
@@ -4,6 +4,43 @@ import urllib
 import urllib2
 import cookielib
 
+ -*- coding:utf-8 -*-
+__author__ = 'Liubai'
+
+"""
+抓取网页可以使用库urllib, urllib2,但鉴于你调用十分麻烦，所以我们第三方的reuqests包，
+一个API十分友好的包，如果你没有安装请自行按照下面命令安装：
+pip install requests
+"""
+
+"""
+希望做一个这样的爬虫类：
+支持登陆
+支持cookies
+支持Proxy
+支持多线程
+支撑压缩传输
+"""
+
+import requests
+import time
+
+proxy ={'http': 'http://109.120.22.4:8080'}
+class spider(object):
+    def __init__(self, url, headers, data, proxy, threads):
+        self.headers = {
+            #伪装成浏览器访问
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) Chrome/44.0.2403.157',
+            #反盗链
+            'Referer': url,
+            #文件以压缩格式传输
+            'Accept-Encoding': 'gzip, deflate'
+        }
+        self.headers.update(headers)
+        self.proxy = proxy
+        self.data = data
+        self.threads = threads
+
 def get_page(url):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1)'
